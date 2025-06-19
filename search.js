55c1e5cd68c6833210c571b3682b098a322ff0b2
Robin <robinghimire098@gmail.com>
2025-06-19 13:22:44 +0545

    search.js

--- search.js
+++ search.js
@@ -1,4 +1,4 @@
-const API_KEY = '1cf50e6248dc270629e802686245c2c8';
+const API_KEY = 'dda93467460f30c868fbb4400c1049e4';
 const BASE_URL = 'https://api.themoviedb.org/3';
 const IMAGE_BASE_URL = 'https://image.tmdb.org/t/p/w500';
 
